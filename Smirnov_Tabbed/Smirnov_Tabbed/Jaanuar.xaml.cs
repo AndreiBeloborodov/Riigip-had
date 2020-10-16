9b9681e2fba798603c7169744122d4b8ff0a4d67
opilane <opilane@HP-CZC6297B8H>
2020-10-16 12:33:36 +0300

    Riigipüha

--- Smirnov_Tabbed/Smirnov_Tabbed/Jaanuar.xaml.cs
+++ Smirnov_Tabbed/Smirnov_Tabbed/Jaanuar.xaml.cs
@@ -15,6 +15,24 @@ namespace Smirnov_Tabbed
         public Jaanuar()
         {
             InitializeComponent();
-        }
-    }
-}
+            Grid grd = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition { Height = new GridLenght(1, GridUnitType.Star) },
+                    new RowDefinition { Height = new GridLenght(3, GridunitType.Star) },
+                    new RowDefinition { Height = new GridLenght(2, GridunitType.Star) }
+                },
+                ColumnDefinitions =
+                {
+                    new RowDefinition { Width = new GridLenght(1, GridunitType.Star) }
+                }
+            };
+            Label nimetus = new Label { Text = "Jaanuari pilt", FontSize = 30 };
+            Image img = new Image { Source = "Январь.jpg" };
+            Label kirjeldus = new Label { Text = "Uusaasta on püha, mil tähistatakse uue kalendriaasta algust. Lääneriikides, kus kasutatakse Gregoriuse kalendrit, on uusaasta 1. jaanuaril." };
+            grd.children.add(nimetus, 0, 0);
+            grd.children.add(img, 0, 1);
+            grd.children.add(kirjeldus, 0, 2);
+            content = grd;
+            {
